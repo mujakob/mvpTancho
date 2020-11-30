fd4a8f4969f34c428c3528dd0bc4b32aa0e560c3
mujakob <jakob.muller@chalmers.se>
2020-11-30 10:30:07 +0100

    first commit

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+from core.routes import core_router
+from pets.routes import pets_router
+from config import config
+
+app = FastAPI()
+
+
+app.include_router(
+    pets_router,
+    prefix="/pets",
+    tags=["pets"],
+    responses={404: {"description": "Not found"}},
+)
+
+app.include_router(
+    core_router,
+    tags=["core"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@app.on_event("startup")
+async def app_startup():
+    """
+    Do tasks related to app initialization.
+    """
+    # This if fact does nothing its just an example.
+    config.load_config()
+
+
+@app.on_event("shutdown")
+async def app_shutdown():
+    """
+    Do tasks related to app termination.
+    """
+    # This does finish the DB driver connection.
+    config.close_db_client()
